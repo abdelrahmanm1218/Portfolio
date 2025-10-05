535db95c2cbaf50a7a143f3530db02a5879ec861
ِAbdelrahman Mahmoud <abdelrahmannali14@gmail.com>
2025-10-05 14:24:10 +0300

    minor edits in nav spacing

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -24,58 +24,60 @@ onMounted(()=> {
 <template>
 
   <v-app-bar class="px-4" :elevation="1" rounded color="background">
-  <template v-slot:append>
-    <v-app-bar-nav-icon v-if="!smAndUp" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-  </template>
+    
+    <template v-slot:append>
+      <v-app-bar-nav-icon  v-if="!smAndUp" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </template>
 
   <!-- Logo on the left -->
-  <div class="logo-container d-flex align-center cursor-pointer" @click="$router.push('/')">
-          <v-img
-        src="/am-logo.png"
-        alt="Logo"
-        width="48"
-        height="48"
-        class="rounded-circle"
-        cover
-      />
-  </div>
+    <div class="logo-container d-flex align-center cursor-pointer ml-4" @click="$router.push('/')">
+            <v-img
+          src="/am-logo.png"
+          alt="Logo"
+          width="48"
+          height="48"
+          class="rounded-circle"
+          cover
+        />
+    </div>
 
-  <!-- Spacer to push navigation to center -->
-  <v-spacer />
-  <v-spacer/>
+    <!-- Spacer to push navigation to center -->
+    <v-spacer />
+    <v-spacer/>
 
-  <!-- Navigation Links - Centered -->
-  <div v-if="smAndUp" class="d-flex">
-    <v-btn
-      v-for="link in links"
-      :key="link.text"
-      variant="text"
-      rounded="lg"
-      :to="link.to"
-      style="color:var(--color-text)"
-      class="nav-btn"
+    <!-- Navigation Links - Centered -->
+    <div v-if="smAndUp" class="d-flex">
+      <v-btn
+        v-for="link in links"
+        :key="link.text"
+        variant="text"
+        rounded="lg"
+        :to="link.to"
+        style="color:var(--color-text)"
+        class="nav-btn"
+      >
+        {{ link.text }}
+      </v-btn>
+    </div>
+
+    <!-- Spacer to push contact button to the right -->
+    <v-spacer />
+    <v-spacer />
+
+    <!-- CTA Button on the right -->
+    <v-btn 
+      v-if="smAndUp" 
+      color="accent"  
+      rounded="lg" 
+      variant="elevated" 
+      href="mailto:abdelrahmannali14@gmail.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Send email to abdelrahmannali14@gmail.com"
+      class="mr-4"
     >
-      {{ link.text }}
+      Contact
     </v-btn>
-  </div>
-
-  <!-- Spacer to push contact button to the right -->
-  <v-spacer />
-  <v-spacer />
-
-  <!-- CTA Button on the right -->
-  <v-btn 
-    v-if="smAndUp" 
-    color="accent"  
-    rounded="lg" 
-    variant="elevated" 
-    href="mailto:abdelrahmannali14@gmail.com"
-    target="_blank"
-    rel="noopener noreferrer"
-    aria-label="Send email to abdelrahmannali14@gmail.com"
-  >
-    Contact
-  </v-btn>
   
 </v-app-bar>
 
